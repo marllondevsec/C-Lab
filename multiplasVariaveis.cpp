722ba31713aadcf4a832e222f378a39c3ad257dc
Root <ghostkernel@Kali>
2025-12-13 00:56:09 -0300

    constantes

---
+++ multiplasVariaveis.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+#define pi 2.1415
+#define print cout << "escrita printada com sucesso";
+
+
+int main(){
+
+	int vidas=3, tiros=500, life=100;
+
+
+		cout << pi << endl;
+
+
+
+
+		print
+
+
+
+
+return 0;
+
+}
