9161bfd58220bf08251d810e5151bc7ec468bc2a
marllondevsec <marllondevsec.comercial@gmail.com>
2025-12-20 17:13:16 -0300

    matriz bidimensional

---
+++ Codigos/MatrizInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main (){
+
+	int matriz [3][4];
+	int x,y;for(x=0;x<3;x++){for(y=0;y<4;y++){
+
+	std::cin >> matriz[x][y];
+
+}}
+
+for(x=0;x<3;x++){for(y=0;y<4;y++){
+
+        std::cout << matriz[x][y] <<  " ";} std::cout << "\n";} 
+
+
+return 0;}
